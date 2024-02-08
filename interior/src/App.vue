769bfd93494c6295174a5be73498096d492f3512
stDem <61629062+stDem@users.noreply.github.com>
2024-02-08 21:33:34 +0300

    questions

---
+++ interior/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header></Header>
+  
+</template>
+
+<script>
+import Header from "./components/Header.Vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ interior/src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="banner"></section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  background: url("/img/banner.png"), lightgray 50% / cover no-repeat;
+  width: 100%;
+  height: 30vh;
+  background-size: cover;
+}
+</style>
